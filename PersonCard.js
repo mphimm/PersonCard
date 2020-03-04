2eccc3dff837e4a12b633b9734bef1dd8499c1be
Millie Phimmasone <59859457+mphimm@users.noreply.github.com>
2020-03-04 09:46:44 -0700

    Create PersonCard.js

---
+++ PersonCard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class PersonCard extends Component {
+    render(){
+        const {firstName, lastName, age, haircolor} = this.props;
+        return(
+            <div>
+                <h1>{lastName}, {firstName}</h1>
+                <p>Age: {age}</p>
+                <p>Hair Color: {haircolor}</p>
+            </div>
+  
+        );
+    }
+}
+
+export default PersonCard;
